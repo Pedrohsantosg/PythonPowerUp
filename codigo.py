16026ebb3a788d1e4fe8ac4a85471dcf428dfb73
Pedrohsantosg <pedro.henrique.gomes@maisunifacisa.com.br>
2024-03-08 15:14:16 -0300

    exchange and variable

--- codigo.py
+++ codigo.py
@@ -1,58 +1,51 @@
-# Passo a passo do projeto
-# Passo 1: Entrar no sistema da empresa
-    # https://dlp.hashtagtreinamentos.com/python/intensivao/login
-# pip install pyautogui 
+
 
 import pyautogui
 import time
 
-# pyautogui.write -> escrever um texto
-# pyautogui.press -> apertar 1 tecla
-# pyautogui.click -> clicar em algum lugar da tela
-# pyautogui.hotkey -> combinação de teclas
+
 pyautogui.PAUSE = 0.4
 
-# abrir o navegador (chrome)
+
 pyautogui.press("win")
 pyautogui.write("chrome")
 pyautogui.press("enter")
 
-# entrar no link 
+
 link = "https://dlp.hashtagtreinamentos.com/python/intensivao/login"
 pyautogui.write(link)
 pyautogui.press("enter")
 time.sleep(3)
 
-# Passo 2: Fazer login
-# selecionar o campo de email
+
 pyautogui.click(x=1010, y=596)
-# escrever o seu email
+
 pyautogui.write("cleiton.macedo@gmail.com")
-pyautogui.press("tab") # passando pro próximo campo
-pyautogui.write("lovedarlin")
-pyautogui.press("tab") # clique no botao de login
+pyautogui.press("tab") 
+pyautogui.write("Macedao")
+pyautogui.press("tab") 
 pyautogui.press("enter")
 time.sleep(3)
 
-# Passo 3: Importar a base de produtos pra cadastrar
+
 import pandas as pd
 
 tabela = pd.read_csv("produtos.csv")
 
 print(tabela)
 
-# Passo 4: Cadastrar um produto
+
 for linha in tabela.index:
     
-# clicar no campo de código
+
     pyautogui.click(x=771, y=457)
-# pegar da tabela o valor do campo que a gente quer preencher
+
     codigo = tabela.loc[linha, "codigo"]
-# preencher o campo
+
     pyautogui.write(str(codigo))
-     # passar para o proximo campo
+ 
     pyautogui.press("tab")
-#  preencher o campo
+
     pyautogui.write(str(tabela.loc[linha, "marca"]))
     pyautogui.press("tab")
     pyautogui.write(str(tabela.loc[linha, "tipo"]))
@@ -67,7 +60,7 @@ for linha in tabela.index:
     if not pd.isna(obs):
         pyautogui.write(str(tabela.loc[linha, "obs"]))
     pyautogui.press("tab")
-    pyautogui.press("enter") # cadastra o produto (botao enviar)
-    # dar scroll de tudo pra cima
+    pyautogui.press("enter")
+
     pyautogui.scroll(5000)
-    # Passo 5: Repetir o processo de cadastro até o fim
+
